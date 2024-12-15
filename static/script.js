adfd2fdf578d0e0dbc0ddc75050ae9c792ea8703
breadbrawlstars <brandon.lin.au@gmail.com>
2024-12-15 21:53:38 +1100

    Added JavaScript functionalities

--- static/script.js
+++ static/script.js
@@ -19,3 +19,25 @@ window.addEventListener('beforeinstallprompt', (e) => {
     });
   });
 });
+
+
+const inactivityTime = 300000; // 5 * 60 * 1000
+const redirectUrl = "/";
+let inactivityTimer;
+function resetInactivityTimer() {
+    clearTimeout(inactivityTimer);
+    inactivityTimer = setTimeout(() => {
+        window.location.href = redirectUrl;
+    }, inactivityTime);
+}
+document.addEventListener("mousemove", resetInactivityTimer);
+document.addEventListener("keydown", resetInactivityTimer);
+document.addEventListener("scroll", resetInactivityTimer);
+document.addEventListener("click", resetInactivityTimer);
+resetInactivityTimer();
+
+function validateField(f){
+  if(f.value.length == 0){
+      alert("Please enter data in the "+f.name+" field");
+  }
+}
